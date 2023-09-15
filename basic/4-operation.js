c4f31ce67f22641a7bc0a98731c0ddc59c8df8a6
Devendra <drvishwakarma1910@gmail.com>
2023-09-15 19:14:40 +0530

    operation

---
+++ basic/4-operation.js
@@ -0,0 +1,38 @@
+let value = 3
+//console.log(value );
+let nagValue = - value
+//console.log(nagValue);
+
+/* 
+    console.log(2+2);
+    console.log(5-2);
+    console.log(5*2);
+    console.log(10%2);
+    console.log(10/2);
+    console.log(2**3);      // 2 to the power 3 
+*/
+  
+/*
+    let str1 = "Hello "
+    let str2 = "Devendra"
+    let str3 = str1+str2
+    console.log(str3); 
+*/
+
+/*
+console.log(1+2);              // => 3
+console.log("1" + 2);          // => 12 
+console.log(1 + "2");          // => 12 
+console.log("1" + "2");        // => 12 
+console.log("1" + 2 + 2);      // => 122 
+console.log(1 + 2 + "2");      // => 32 
+*/
+
+/*
+console.log(true);  // true
+console.log(+true); // 1
+*/
+
+let x = 100;
+console.log(x++);
+console.log(++x);
