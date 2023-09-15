3e88c0c87cadc9f6e096beef1f38e4f5bc8a190e
Devendra <drvishwakarma1910@gmail.com>
2023-09-15 18:11:19 +0530

    dataType

---
+++ basic/3-dataTypes.js
@@ -0,0 +1,8 @@
+let name = "Devendra"     //String
+let age  = 22             // int
+let isLoggedIn = false    // boolean
+
+// if number is greater then 2^53 then it should be store in bigint
+// null => standalone value
+// undefined => if you declared the variable but not assigne anything 
+// synbol => unique identify (mostly used in React.JS)
