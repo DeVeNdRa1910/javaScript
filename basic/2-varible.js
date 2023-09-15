a91fd13dc86f82d43dd5b3b4ed91290facfa7e7a
Devendra <drvishwakarma1910@gmail.com>
2023-09-15 18:10:32 +0530

    variable

---
+++ basic/2-varible.js
@@ -0,0 +1,21 @@
+const accId = 148321
+let accEmail = "abd@gmail.com"
+var accPassword = " 1234545"
+accCity = "BHOPAL"
+let accState
+
+// accId = 61243  this not allow with const
+/*
+prefer not to use var 
+because of isuue in block scope and functional scope
+*/
+
+console.log([accId , accEmail , accPassword , accCity , accState]);
+
+
+accEmail = " dcba@gmail.com"
+accPassword = " 0987654321"
+accCity = " Bengluru"
+
+
+console.log([accId , accEmail , accPassword , accCity ,accState]);
